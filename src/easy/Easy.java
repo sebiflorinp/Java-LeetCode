251bccdd5c9f9324128d3aa9ac81a55baadd441f
sebifp <sebiflorinp@gmail.com>
2024-04-04 13:38:43 +0300

    ADD binary search solution

--- src/easy/Easy.java
+++ src/easy/Easy.java
@@ -107,4 +107,23 @@ public class Easy {
         }
         return result;
     }
+
+    public int search(int[] nums, int target) {
+        int start = 0;
+        int end = nums.length - 1;
+        int targetPosition = -1;
+
+        while (start <= end) {
+            int middle = (start + end) / 2;
+            if (nums[middle] == target) {
+                targetPosition = middle;
+                return targetPosition;
+            } else if (nums[middle] < target) {
+                start = middle + 1;
+            } else {
+                end = middle - 1;
+            }
+        }
+        return targetPosition;
+    }
 }
