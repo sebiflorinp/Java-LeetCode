82a8d1c5846058c36fa555476819531a98e898b2
sebifp <sebiflorinp@gmail.com>
2024-04-06 11:20:09 +0300

    ADD maximum profit solution

--- src/easy/Easy.java
+++ src/easy/Easy.java
@@ -191,4 +191,17 @@ public class Easy {
 
         return paranthesisStack.empty();
     }
+
+    public int maxProfit(int[] prices) {
+        int buy = prices[0];
+        int profit = 0;
+        for (int i = 1; i < prices.length; i++) {
+            if (buy > prices[i]) {
+                buy = prices[i];
+            } else if (prices[i] - buy > profit) {
+                profit = prices[i] - buy;
+            }
+        }
+        return profit;
+    }
 }
